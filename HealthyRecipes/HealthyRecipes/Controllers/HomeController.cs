b7c46d495cb44f9de7391aa9a9bdd8dadcafc6f1
ViktoriaBPetrova <vickiki036@gmail.com>
2025-11-06 11:26:58 +0200

    Added a solution - Web App with MVC

---
+++ HealthyRecipes/HealthyRecipes/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using HealthyRecipes.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthyRecipes.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
