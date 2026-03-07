024b3edbec3a244c27a91fe99ab93fabed09c643
ViktoriaBPetrova <vickiki036@gmail.com>
2026-03-07 20:45:51 +0200

    Added viewmodels for user

---
+++ HealthyRecipes/HealthyRecipes/Models/User/AllergyViewModel.cs
@@ -0,0 +1,8 @@
+namespace HealthyRecipes.Web.Models.User
+{
+    public class AllergyViewModel
+    {
+        public Guid IngredientId { get; set; }
+        public string IngredientName { get; set; } = null!;
+    }
+}
