0a49ab125d1f10e8aa237f7c9ce2bde72d055692
ViktoriaBPetrova <vickiki036@gmail.com>
2026-03-26 05:02:48 +0200

    Splitted one controller file into 2 for better readability

---
+++ HealthyRecipes/HealthyRecipes/Controllers/CategoryController.cs
@@ -0,0 +1,63 @@
+﻿using HealthyRecipes.Data.Entities;
+using HealthyRecipes.Services.Categories;
+using HealthyRecipes.Web.ViewModels.Category;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using HealthyRecipes.Web.ViewModels.Category;
+
+namespace HealthyRecipes.Web.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategory _categoryService;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public CategoryController(ICategory categoryService, UserManager<ApplicationUser> userManager)
+        {
+            _categoryService = categoryService;
+            _userManager = userManager;
+        }
+
+        // GET: /Category
+        [AllowAnonymous]
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _categoryService.GetAllCategoriesAsync();
+            var vm = new CategoryIndexViewModel
+            {
+                Categories = categories.Select(c => new CategoryViewModel
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                    RecipeCount = c.RecipeCategories?.Count() ?? 0
+                })
+            };
+            return View(vm);
+        }
+
+        // GET: /Category/Create
+        public IActionResult Create() => View(new CreateCategoryViewModel());
+
+        // POST: /Category/Create
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreateCategoryViewModel vm)
+        {
+            if (!ModelState.IsValid) return View(vm);
+
+            var user = await _userManager.GetUserAsync(User);
+            var category = new Category { Name = vm.Name, CreatedBy = user!.Id };
+            await _categoryService.CreateCategoryAsync(category);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // POST: /Category/Delete/{id}
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await _categoryService.SoftDeleteCategoryAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- HealthyRecipes/HealthyRecipes/Controllers/IngredientController.cs
+++ HealthyRecipes/HealthyRecipes/Controllers/IngredientController.cs
@@ -3,7 +3,6 @@ using HealthyRecipes.Data.Enums;
 using HealthyRecipes.Services.Allergies;
 using HealthyRecipes.Services.Categories;
 using HealthyRecipes.Services.Ingredients;
-using HealthyRecipes.Web.ViewModels.Category;
 using HealthyRecipes.Web.ViewModels.Ingredient;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -101,59 +100,6 @@ namespace HealthyRecipes.Web.Controllers
             else
                 await _allergyService.AddAllergyAsync(user.Id, id);
 
-            return RedirectToAction(nameof(Index));
-        }
-    }
-
-    [Authorize(Roles = "Admin")]
-    public class CategoryController : Controller
-    {
-        private readonly ICategory _categoryService;
-        private readonly UserManager<ApplicationUser> _userManager;
-
-        public CategoryController(ICategory categoryService, UserManager<ApplicationUser> userManager)
-        {
-            _categoryService = categoryService;
-            _userManager = userManager;
-        }
-
-        // GET: /Category
-        [AllowAnonymous]
-        public async Task<IActionResult> Index()
-        {
-            var categories = await _categoryService.GetAllCategoriesAsync();
-            var vm = new CategoryIndexViewModel
-            {
-                Categories = categories.Select(c => new CategoryViewModel
-                {
-                    Id = c.Id,
-                    Name = c.Name,
-                    RecipeCount = c.RecipeCategories?.Count() ?? 0
-                })
-            };
-            return View(vm);
-        }
-
-        // GET: /Category/Create
-        public IActionResult Create() => View(new CreateCategoryViewModel());
-
-        // POST: /Category/Create
-        [HttpPost, ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(CreateCategoryViewModel vm)
-        {
-            if (!ModelState.IsValid) return View(vm);
-
-            var user = await _userManager.GetUserAsync(User);
-            var category = new Category { Name = vm.Name, CreatedBy = user!.Id };
-            await _categoryService.CreateCategoryAsync(category);
-            return RedirectToAction(nameof(Index));
-        }
-
-        // POST: /Category/Delete/{id}
-        [HttpPost, ValidateAntiForgeryToken]
-        public async Task<IActionResult> Delete(Guid id)
-        {
-            await _categoryService.SoftDeleteCategoryAsync(id);
             return RedirectToAction(nameof(Index));
         }
     }
